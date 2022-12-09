ada06751d9e7b0684c2593660c636d4b6edce740
aman saraf <amansaraf99@gmail.com>
2022-12-09 03:01:18 -0800

    Code seems error free, need to fix makefile

--- src/image_inference.cpp
+++ src/image_inference.cpp
@@ -54,23 +54,6 @@
 #include <cv_bridge/cv_bridge.h>
 
 /* -------------------------------------------------------------------------- */
-/*                           CONSTANTS & PARAMETERS                           */
-/* -------------------------------------------------------------------------- */
-const std::string ENGINE_PATH = "/home/roar/ART/perception/model_trials/NVIDIA_AI_IOT_tensorrt_yolov7/yolo_deepstream/tensorrt_yolov7/build/yolov7PTQ.engine";
-
-/* -------------------------------------------------------------------------- */
-/*                              HELPER FUNCTIONS                              */
-/* -------------------------------------------------------------------------- */
-
-// TODO: FIX THIS for a static implementation or dynamic ros2 implementation.
-// std::string parse_model_path(argsParser& cmdLine) {
-//     const char* engine_path_str = cmdLine.ParseString("engine");
-//     std::string engine_path;
-//     if (engine_path_str) engine_path = std::string(engine_path_str);
-//     return engine_path;
-// }
-
-/* -------------------------------------------------------------------------- */
 /*                                 ROS2 NODE                                  */
 /* -------------------------------------------------------------------------- */
 using namespace std::chrono_literals;
@@ -78,23 +61,18 @@ using namespace std::chrono_literals;
 class YOLOv7InferenceNode : public rclcpp::Node
 {
   public:
-    rclcpp::Subscription<sensor_msgs::msg::Image>::SharedPtr image_sub_;
-    rclcpp::Publisher<sensor_msgs::msg::Image>::SharedPtr image_pub_;
-    
     /* ---------------------------- YOLOv7 Init Stuff --------------------------- */
-    Yolov7 yolov7;
-    yolov7 = Yolov7(std::string(ENGINE_PATH));
-    std::vector<cv::Mat> bgr_imgs;
+    std::string engine_path_;
+    std::unique_ptr<Yolov7> yolov7_;
 
-    YOLOv7InferenceNode()
-    : Node("yolov7_inference_node")
+    YOLOv7InferenceNode() : Node("yolov7_inference_node")
     {
       /* ------------------------------- QOS Profile ------------------------------ */
       rmw_qos_profile_t qos_profile = rmw_qos_profile_sensor_data;
       auto sensor_msgs_qos = rclcpp::QoS(rclcpp::QoSInitialization(qos_profile.history, 1), qos_profile);
 
       /* --------------------------- Image Subscription --------------------------- */
-      image_sub_ = this->create_subscription<sensor_msgs::msg::Image>(
+      auto image_sub_ = this->create_subscription<sensor_msgs::msg::Image>(
         "/vimba_front_left_center/image",
         sensor_msgs_qos,
         std::bind(&YOLOv7InferenceNode::image_callback, this, std::placeholders::_1)
@@ -105,10 +83,17 @@ class YOLOv7InferenceNode : public rclcpp::Node
         "/vimba_front_left_center/det_image",
         sensor_msgs_qos
       );
+
+      engine_path_ = "/home/roar/ART/perception/model_trials/NVIDIA_AI_IOT_tensorrt_yolov7/yolo_deepstream/tensorrt_yolov7/build/yolov7PTQ.engine";
+    
+      yolov7_ = std::make_unique<Yolov7>(engine_path_);
     }
 
 
 private:
+  rclcpp::Subscription<sensor_msgs::msg::Image>::SharedPtr image_sub_;
+  rclcpp::Publisher<sensor_msgs::msg::Image>::SharedPtr image_pub_;
+
   void image_callback(const sensor_msgs::msg::Image::SharedPtr msg)
   {
     /* ------------------ Receive msg and convert to cv2 format ----------------- */
@@ -124,24 +109,22 @@ private:
     }
 
     /* -------------------------- Preprocess the images ------------------------- */
-    this->bgr_imgs.push_back(cv_ptr->image);
-    (this->yolov7).preProcess(this->bgr_imgs);
+    std::vector<cv::Mat> bgr_imgs;
+    bgr_imgs.push_back(cv_ptr->image);
+    yolov7_->preProcess(bgr_imgs);
 
     /* ------------------------------ Run Inference ----------------------------- */
-    yolov7.infer();
+    yolov7_->infer();
 
     /* -------------------------- Run NMS & PostProcess ------------------------- */
-    std::vector<std::vector<std::vector<float>>> nmsresults = yolov7().PostProcess();
+    std::vector<std::vector<std::vector<float>>> nmsresults = yolov7_->PostProcess();
 
     for(int j =0; j < nmsresults.size();j++){
         // TODO: Publish here!
         Yolov7::DrawBoxesonGraph(bgr_imgs[j],nmsresults[j]);
 
         cv_ptr->image = bgr_imgs[j];
-        image_pub_.publish(cv_ptr->toImageMsg());
-        // std::string output_path = img_paths[j] + "detect" + std::to_string(j)+".jpg";      
-        // cv::imwrite(output_path, bgr_imgs[j]);
-        // std::cout<<"detectec image written to: "<<output_path<<std::endl;
+        image_pub_->publish(*(cv_ptr->toImageMsg()).get() );
     }
 
 
@@ -151,7 +134,7 @@ private:
 int main(int argc, char * argv[])
 {
   rclcpp::init(argc, argv);
-  rclcpp::spin(std::make_shared<MinimalPublisher>());
+  rclcpp::spin(std::make_shared<YOLOv7InferenceNode>());
   rclcpp::shutdown();
   return 0;
 }
